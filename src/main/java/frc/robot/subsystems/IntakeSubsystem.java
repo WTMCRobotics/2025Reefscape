53ba04b336fc760e83bfd7f61047933298e91445
team6101 <wtmcrobotics@wccnet.edu>
2025-02-17 17:58:55 -0500

    Start Intake Subsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+    // TODO: make the ids good
+    SparkMax pivotMotor = new SparkMax(8, MotorType.kBrushless);
+    SparkMax intakeMotor = new SparkMax(9, MotorType.kBrushless);
+
+    @Override
+    public void periodic() {
+        
+    }
+    public void movePivot(double speed) {
+        pivotMotor.set(speed);
+    }
+    
+    public void stopPivot(double speed) {
+        pivotMotor.set(0);
+
+    public void dealgaenatePivot() {}
+    }
+}
